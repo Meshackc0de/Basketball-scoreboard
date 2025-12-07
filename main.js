aa2c54842e655b3b73afdf76148e561963eb6acd
Meshack John <meshackjohn420@gmail.com>
2025-12-07 17:00:51 +0300

    add scoreboard functionality

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-let scoreEl = document.getElementById("score-el");
+let homeScoreEl = document.getElementById("home-score-el");
 let guestScoreEl = document.getElementById('guest-score-el')
 let homeScore = 0;
 let guestScore = 0;
 
-function addPints(team, points){
+function addPoints(team, points){
     if(team === 'home'){
         homeScore += points;
-        scoreEl.textContent = homeScore;
+        homeScoreEl.textContent = homeScore;
     } else if( team === 'guest'){
         guestScore += points;
         guestScoreEl.textContent = guestScore;
@@ -14,41 +14,11 @@ function addPints(team, points){
 
 }
 
-function plusOne(){
-    homeScore += 1;
-    guestScore += 1;
-    scoreEl.textContent = homeScore;
-    guestScoreEl.textContent = guestScore;
-}
-function plusTwo(){
-    score += 2;
-    scoreEl.textContent = score;
-}
-
-function plusThree(){
-    score += 3;
-    scoreEl.textContent = score;
-}
-
 function resetHomeScore(){
-    score = 0;
-    scoreEl.textContent = score;
+    homeScore = 0;
+    homeScoreEl.textContent = homeScore;
 }
-
-// function addOne(){
-//     score += 1
-//     guestScoreEl.textContent = score;
-// }
-function addTwo(){
-    score += 2
-    guestScoreEl.textContent = score;
-}
-function addThree(){
-    score += 3
-    guestScoreEl.textContent = score;
-}
-
 function resetGuestScore(){
-    score = 0;
-    guestScoreEl.textContent = score;
+    guestScore = 0;
+    guestScoreEl.textContent = guestScore;
 }
